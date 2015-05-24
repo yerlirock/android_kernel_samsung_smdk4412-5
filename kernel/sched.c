c62b425fac0225f59490d6ad1c6fa947ae224b08
psndna88 <psndna88@gmail.com>
2015-05-24 19:11:55 +0530

    Implemented intelli_plug (arter97)
    Revert "scheduler: compute time-average nr_running per run-queue"
    
    Conflicts:
    	arch/arm/Makefile
    	kernel/sched.c
    
    Change-Id: Icd2a4f2c0276abc34888f7bfd3dd9a1435d73ae4

--- kernel/sched.c
+++ kernel/sched.c
@@ -1874,7 +1874,10 @@ static const struct sched_class rt_sched_class;
 static void inc_nr_running(struct rq *rq)
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> 2391dc6... Implemented intelli_plug (arter97)
 #ifdef CONFIG_INTELLI_PLUG
 	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
 #endif
@@ -1884,6 +1887,9 @@ static void inc_nr_running(struct rq *rq)
 	nr_stats->ave_nr_running = do_avg_nr_running(rq);
 	nr_stats->nr_last_stamp = rq->clock_task;
 #endif
+<<<<<<< HEAD
+>>>>>>> 2391dc6... Implemented intelli_plug (arter97)
+=======
 >>>>>>> 2391dc6... Implemented intelli_plug (arter97)
 	rq->nr_running++;
 #ifdef CONFIG_INTELLI_PLUG
@@ -1894,7 +1900,10 @@ static void inc_nr_running(struct rq *rq)
 static void dec_nr_running(struct rq *rq)
 {
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> 2391dc6... Implemented intelli_plug (arter97)
 #ifdef CONFIG_INTELLI_PLUG
 	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
 #endif
@@ -1904,6 +1913,9 @@ static void dec_nr_running(struct rq *rq)
 	nr_stats->ave_nr_running = do_avg_nr_running(rq);
 	nr_stats->nr_last_stamp = rq->clock_task;
 #endif
+<<<<<<< HEAD
+>>>>>>> 2391dc6... Implemented intelli_plug (arter97)
+=======
 >>>>>>> 2391dc6... Implemented intelli_plug (arter97)
 	rq->nr_running--;
 #ifdef CONFIG_INTELLI_PLUG
